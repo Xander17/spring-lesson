3e7c29c3120df33234d5995a76f2e8eee5eb28fc
Cromag <alseko_2001@mail.ru>
2020-05-19 17:58:17 +0300

    camera init

---
+++ camera/src/main/java/ru/geekbrains/Config.java
@@ -0,0 +1,10 @@
+package ru.geekbrains;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.geekbrains")
+public class Config {
+
+}
